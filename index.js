26f371a010b722f350eeae3fc0266285e8066013
TinekeV <tinekevlieger@gmail.com>
2021-01-27 11:24:29 +0100

    jest-test-oefening

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // OEFENING 1
 // schrijf een functie die het oppervlak van een bepaalde ruimte teruggeeft
 
-function sizeRoom(length, width) {
-    return length * width
+function sizeRoom() {
+
 }
 
 
@@ -11,13 +11,10 @@ function sizeRoom(length, width) {
 // OEFENING 2
 // schrijf een functie die twee arrays samenvoegt en omgezet wordt naar een string
 
-function arrayToString(array1, array2) {
-    const fullArray = array1.concat(array2);
-    return fullArray.join(', ');
-}
+function arrayToString() {
 
-// om te controleren of de functie werkt
-console.log(arrayToString(['trap', 'plant', 'hoed'], ['stoel', 'bank']))
+};
+
 
 module.exports = {
     sizeRoom: sizeRoom,
--- opdracht-omschrijving.md
+++ opdracht-omschrijving.md
@@ -1,12 +1,4 @@
-# Maak de oefening
+Schrijf de functies zo dat de testen slagen. 
+De oefeningen bevatten een berekening, arrays en strings. 
 
-
-Volg de volgende stappen om aan de slag te gaan:
-
-1. Clone de repo naar je editor
-2. 'cd <reponaam.>'
-2. Run ' **npm install** '
-3. Run ' **npm jest --watch** ' of ' **npx jest** ' om testen te starten
-4. Zoek de fout(en) op en fix deze!
-
-Succes!
+Succes! 
